f97dd77551c67f055f742553e4db6f449ec94592
charlie.crean <charlie.crean@r3.com>
2021-08-18 14:32:00 +0100

    Update cordapp configs to be workflow cordapps instead of contract cordapps. Add some extra unit tests.

--- modules/tokens-builder/src/test/kotlin/com/r3/corda/lib/tokens/builder/FungibleTokenBuilderTest.kt
+++ modules/tokens-builder/src/test/kotlin/com/r3/corda/lib/tokens/builder/FungibleTokenBuilderTest.kt
@@ -20,7 +20,8 @@ class FungibleTokenBuilderTest {
     private lateinit var mockIssuer: Party
     private lateinit var mockHolder: AbstractParty
     private lateinit var mockHashingService: HashingService
-    private lateinit var issuedTokenTypeAmountMock: Amount<IssuedTokenType>
+    private lateinit var mockIssuedTokenType: IssuedTokenType
+    private lateinit var mockIssuedTokenTypeAmount: Amount<IssuedTokenType>
 
     private lateinit var builder: FungibleTokenBuilder
 
@@ -40,7 +41,14 @@ class FungibleTokenBuilderTest {
         }
 
         mockHolder = mockk()
-        issuedTokenTypeAmountMock = mockk()
+        mockIssuedTokenType = mockk {
+            every { tokenType } returns mockTokenType
+            every { issuer } returns mockIssuer
+        }
+        mockIssuedTokenTypeAmount = mockk {
+            every { token } returns mockIssuedTokenType
+            every { quantity } returns 1
+        }
         mockHashingService = mockk {
             every { digestLength(any()) } returns 64
         }
@@ -67,7 +75,7 @@ class FungibleTokenBuilderTest {
             .ofTokenType(mockTokenType)
 
         // Add issued token type which isn't allowed combined with the above builder functions
-        builder.withIssuedTokenTypeAmount(issuedTokenTypeAmountMock)
+        builder.withIssuedTokenTypeAmount(mockIssuedTokenTypeAmount)
 
         // Assert exception thrown when building amount token type
         assertThrows<TokenBuilderException> { builder.buildAmountTokenType() }
@@ -113,7 +121,7 @@ class FungibleTokenBuilderTest {
             .issuedBy(mockIssuer)
 
         // Add issued token type which isn't allowed combined with the above builder functions
-        builder.withIssuedTokenTypeAmount(issuedTokenTypeAmountMock)
+        builder.withIssuedTokenTypeAmount(mockIssuedTokenTypeAmount)
 
         // Assert exception thrown when building amount token type
         assertThrows<TokenBuilderException> { builder.buildAmountIssuedTokenType() }
@@ -144,5 +152,68 @@ class FungibleTokenBuilderTest {
         assertEquals(1, result.amount.quantity)
         assertEquals(mockTokenType, result.amount.token.tokenType)
         assertEquals(mockIssuer, result.amount.token.issuer)
+    }
+
+    @Test
+    fun `buildFungibleToken has expected output when builder is set up correctly with pre-built issued token type amount`() {
+        // Set up FungibleToken properties necessary to build amount token type
+        builder.withIssuedTokenTypeAmount(mockIssuedTokenTypeAmount)
+            .heldBy(mockHolder)
+
+        // Assert Amount of type TokenType is correctly constructed
+        val result =  builder.buildFungibleToken(mockHashingService)
+        assertEquals(mockHolder, result.holder)
+        assertEquals(mockIssuer, result.issuer)
+        assertEquals(1, result.amount.quantity)
+        assertEquals(mockTokenType, result.amount.token.tokenType)
+        assertEquals(mockIssuer, result.amount.token.issuer)
+    }
+
+    @Test
+    fun `buildFungibleToken fails if holder is not set`() {
+        builder.withAmount(1)
+            .ofTokenType(mockTokenType)
+            .issuedBy(mockIssuer)
+
+        assertThrows<TokenBuilderException> { builder.buildFungibleToken(mockHashingService) }
+    }
+
+    @Test
+    fun `Builder can use Long amounts`() {
+        // Set up FungibleToken properties necessary to build amount token type
+        builder.withAmount(1.toLong())
+            .ofTokenType(mockTokenType)
+            .issuedBy(mockIssuer)
+            .heldBy(mockHolder)
+
+        // Assert Amount of type TokenType is correctly constructed
+        val result =  builder.buildFungibleToken(mockHashingService)
+        assertEquals(1, result.amount.quantity)
+    }
+
+    @Test
+    fun `Builder can use Double amounts`() {
+        // Set up FungibleToken properties necessary to build amount token type
+        builder.withAmount(1.toDouble())
+            .ofTokenType(mockTokenType)
+            .issuedBy(mockIssuer)
+            .heldBy(mockHolder)
+
+        // Assert Amount of type TokenType is correctly constructed
+        val result =  builder.buildFungibleToken(mockHashingService)
+        assertEquals(1, result.amount.quantity)
+    }
+
+    @Test
+    fun `Builder can use BigDecimal amounts`() {
+        // Set up FungibleToken properties necessary to build amount token type
+        builder.withAmount(1.toBigDecimal())
+            .ofTokenType(mockTokenType)
+            .issuedBy(mockIssuer)
+            .heldBy(mockHolder)
+
+        // Assert Amount of type TokenType is correctly constructed
+        val result =  builder.buildFungibleToken(mockHashingService)
+        assertEquals(1, result.amount.quantity)
     }
 }
--- modules/tokens-builder/src/test/kotlin/com/r3/corda/lib/tokens/builder/TokenBuilderUtilitiesTest.kt
+++ modules/tokens-builder/src/test/kotlin/com/r3/corda/lib/tokens/builder/TokenBuilderUtilitiesTest.kt
@@ -1,5 +1,7 @@
 package com.r3.corda.lib.tokens.builder
 
+import com.r3.corda.lib.tokens.contracts.states.AbstractToken
+import com.r3.corda.lib.tokens.contracts.states.EvolvableTokenType
 import com.r3.corda.lib.tokens.contracts.states.FungibleToken
 import com.r3.corda.lib.tokens.contracts.states.NonFungibleToken
 import com.r3.corda.lib.tokens.contracts.types.IssuedTokenType
@@ -7,8 +9,15 @@ import com.r3.corda.lib.tokens.contracts.types.TokenType
 import io.mockk.every
 import io.mockk.mockk
 import net.corda.v5.application.identity.AbstractParty
+import net.corda.v5.application.identity.Party
 import net.corda.v5.application.services.crypto.HashingService
+import net.corda.v5.crypto.SecureHash
+import net.corda.v5.ledger.UniqueIdentifier
 import net.corda.v5.ledger.contracts.Amount
+import net.corda.v5.ledger.contracts.BelongsToContract
+import net.corda.v5.ledger.contracts.Contract
+import net.corda.v5.ledger.contracts.TransactionState
+import net.corda.v5.ledger.transactions.LedgerTransaction
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -27,6 +36,7 @@ class TokenBuilderUtilitiesTest {
     private val mockHashingService: HashingService = mockk {
         every { digestLength(any()) } returns 64
     }
+    private val mockNotary: Party = mockk()
 
     @Test
     fun `Create fungible token from amount using infix heldBy and withHashingService`() {
@@ -43,4 +53,46 @@ class TokenBuilderUtilitiesTest {
         assertEquals(mockIssuedTokenType, token.token)
         assertEquals(mockParty, token.holder)
     }
+
+    @Test
+    fun `Create transaction state for abstract token with a given notary`() {
+        val mockToken = mockk<DummyToken>()
+        val transactionState: TransactionState<AbstractToken> = mockToken withNotary mockNotary
+
+        assertEquals(mockToken, transactionState.data)
+        assertEquals(mockNotary, transactionState.notary)
+    }
+
+    @Test
+    fun `Create transaction state for evolvable token type with a given notary`() {
+        val mockEvolvableTokenType: DummyEvolvableToken = mockk()
+        val transactionState: TransactionState<EvolvableTokenType> = mockEvolvableTokenType withNotary mockNotary
+
+        assertEquals(mockEvolvableTokenType, transactionState.data)
+        assertEquals(mockNotary, transactionState.notary)
+    }
 }
+
+
+class TestContract : Contract {
+    override fun verify(tx: LedgerTransaction) {}
+}
+
+@BelongsToContract(TestContract::class)
+class DummyToken(
+    override val holder: AbstractParty,
+    override val issuedTokenType: IssuedTokenType,
+    override val tokenTypeJarHash: SecureHash?
+) : AbstractToken {
+    override fun withNewHolder(newHolder: AbstractParty): AbstractToken {
+        return DummyToken(newHolder, issuedTokenType, tokenTypeJarHash)
+    }
+}
+
+
+@BelongsToContract(TestContract::class)
+class DummyEvolvableToken(
+    override val linearId: UniqueIdentifier,
+    override val maintainers: List<Party>,
+    override val fractionDigits: Int
+) : EvolvableTokenType()
